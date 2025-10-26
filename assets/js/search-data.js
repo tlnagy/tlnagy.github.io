5dbc52a810db87e890cc4aa322cbe59ec177d1e3
tlnagy <tamas@tamasnagy.com>
2025-10-26 23:00:53 +0000

    Deploying to gh-pages from @ tlnagy/tlnagy.github.io@9810b6402f86e5f5d6d73ad7030e97455330c92b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-finally-updated-my-website-sweat-smile-it-s-now-using-the-al-folio-theme-sparkles",
+          title: 'Finally updated my website :sweat_smile:, it’s now using the al-folio theme! :sparkles:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
